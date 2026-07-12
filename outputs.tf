e4eb7061b806c2cbc70d57102d483e690c0e88eb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "api_management_product_tags" {
-  description = "All api_management_product_tag resources"
-  value       = azurerm_api_management_product_tag.api_management_product_tags
-}
 output "api_management_product_tags_api_management_name" {
-  description = "List of api_management_name values across all api_management_product_tags"
-  value       = [for k, v in azurerm_api_management_product_tag.api_management_product_tags : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_product_tags, keyed the same as var.api_management_product_tags"
+  value       = { for k, v in azurerm_api_management_product_tag.api_management_product_tags : k => v.api_management_name }
 }
 output "api_management_product_tags_api_management_product_id" {
-  description = "List of api_management_product_id values across all api_management_product_tags"
-  value       = [for k, v in azurerm_api_management_product_tag.api_management_product_tags : v.api_management_product_id]
+  description = "Map of api_management_product_id values across all api_management_product_tags, keyed the same as var.api_management_product_tags"
+  value       = { for k, v in azurerm_api_management_product_tag.api_management_product_tags : k => v.api_management_product_id }
 }
 output "api_management_product_tags_name" {
-  description = "List of name values across all api_management_product_tags"
-  value       = [for k, v in azurerm_api_management_product_tag.api_management_product_tags : v.name]
+  description = "Map of name values across all api_management_product_tags, keyed the same as var.api_management_product_tags"
+  value       = { for k, v in azurerm_api_management_product_tag.api_management_product_tags : k => v.name }
 }
 output "api_management_product_tags_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_product_tags"
-  value       = [for k, v in azurerm_api_management_product_tag.api_management_product_tags : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_product_tags, keyed the same as var.api_management_product_tags"
+  value       = { for k, v in azurerm_api_management_product_tag.api_management_product_tags : k => v.resource_group_name }
 }
 
